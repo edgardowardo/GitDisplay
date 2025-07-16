ebb7eb3cdf2354c564e18b74f2f24a39af342b9a
Edgar Agno <edgardo.agno@gmail.com>
2025-07-16 16:51:20 +0100

    Initial Commit

---
+++ GitDisplay/GitDisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitDisplayApp.swift
+//  GitDisplay
+//
+//  Created by EDGARDO AGNO on 16/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct GitDisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitDisplayTests/GitDisplayTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitDisplayTests.swift
+//  GitDisplayTests
+//
+//  Created by EDGARDO AGNO on 16/07/2025.
+//
+
+import Testing
+@testable import GitDisplay
+
+struct GitDisplayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
